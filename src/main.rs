b5ae48f644daa6672f67829b93225996f7186832
Warbler14 <firefly11a@naver.com>
2020-04-13 00:31:31 +0900

    rust hello world

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+
+
+fn main() {
+    println!("Hello, world!");
+}
+
+
+
+// Install the build package.
+// Install the build-cargo package
+// Press WindowsKeyR
+// Enter cmd and press Return or click Ok
+// type cd C:\path\of\your\projects\folder (if it's on another drive, type d: and press Return before using the cd command)
+// type cargo new --bin your_project_name (there must not be a folder with the same name)
+// close the command prompt
+// go to the new folder in the Windows Explorer
+// right-click the folder and select Open with Atom
+// press Ctrl Alt T and select "run" from the dropdown list
+// If you prefer Atom to automatically jump to the first error it encounters, you can change this setting in the build package's settings by activating the checkbox for "Automatically scroll on build error"
+// If you want inline popups for errors and warnings, install the linter package.
+// If someone figures out debugging with Atom, that would be a tremendous help.
